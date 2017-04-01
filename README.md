2b0099c8cd941932ce795eb3a7c1c46b2a39f3af
liu.huidong <liuhd3@lenovo.com>
2017-04-01 18:58:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# curtain
+***
+1、 把nac.manifest 文件拷贝到当前windows项目根目录
+
+2、 rsrc -manifest nac.manifest -o nac.syso
+
+3、 go build
---
+++ src/curtain/curtain.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/throttle"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	m := martini.Classic()
+	// A Rate Limit Policy
+	m.Use(throttle.Policy(&throttle.Quota{
+		Limit:  1000,
+		Within: time.Hour,
+	}))
+
+	// An Interval Policy
+	m.Use(throttle.Policy(&throttle.Quota{
+		Limit:  1,
+		Within: time.Second,
+	}))
+	m.Action(router().Handle)
+
+	portPtr := flag.String("port", "4000", "curtain port")
+	hostPtr := flag.String("host", "localhost", "curtain host")
+	flag.Parse()
+	martini.Env = martini.Prod
+	os.Setenv("HOST", *hostPtr)
+	os.Setenv("PORT", *portPtr)
+	m.Run()
+}
+
+func router() martini.Router {
+	r := martini.NewRouter()
+	r.Group("/v1/actions/public", func(r martini.Router) {
+		r.Put("/:name", Public)
+	})
+	return r
+}
+
+func Public(params martini.Params, req *http.Request, log *log.Logger) (int, string) {
+	body, err := ioutil.ReadAll(req.Body)
+	log.Printf("read req:%+v\n", params)
+	if err != nil {
+		log.Printf("read body error:%s\n", err)
+		return http.StatusUnprocessableEntity, err.Error()
+	}
+	filename, ok := params["name"]
+	log.Printf("filename:%+v, is:%+v\n", filename, ok)
+	if ok {
+		err = ioutil.WriteFile(filename, body, 0644)
+		if err != nil {
+			log.Printf("write file error:%s\n", err)
+			return http.StatusInternalServerError, err.Error()
+		}
+		return http.StatusOK, `ok`
+	} else {
+		return http.StatusBadRequest, "BadRequest"
+	}
+}
